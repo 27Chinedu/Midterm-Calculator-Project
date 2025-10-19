c299aa2951ce49253aeb8db43a39002d1c4a91d9
Chinedu Erechukwu <ce65@njit.edu>
2025-10-19 16:39:35 -0400

    Added Initial Project Structure File

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+Simple Calculator Package
+A basic calculator implementation in Python
+"""
+
+__version__ = "0.1.0"
+__author__ = "Chinedu Erechukwu"
+
+from .calculator import Calculator
+
+__all__ = ['Calculator']
